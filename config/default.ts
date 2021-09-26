c23372768b9608dfa039465565f4e74f85104b18
rexdougie <bitokdouglas@gmail.com>
2021-09-26 20:21:02 +0100

    IntelliSOFT-BMI Backend MongoDB Connection

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+    port: 1337,
+    host: "localhost",
+    dbUrl: "mongodb+srv://cluster0.bchie.mongodb.net/myFirstDatabase",
+};
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello There!')
+import express from 'express';
+import config from 'config';
+
+const port = config.get("port") as number;
+const host = config.get("host") as string;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, host, () => {
+    console.log(`Server listening at http://${host}:${port}`);
+});
